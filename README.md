28a7a102548d3fb02088dbd02675ab8ecff2553e
Andrew Schmelyun <schmelyun@gmail.com>
2022-09-08 21:27:08 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GitHub Actions Docker - Compile Assets
+
+This is an example project for the "Using GitHub Actions with Docker Containers" video. This uses Docker for one step in a larger job to compile PHP and JavaScript assets in the codebase.
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    purge: [
+        './resources/views/**/*.php',
+        './resources/views/**/*.html',
+        './resources/assets/js/**/*.vue',
+        './resources/content/**/*.html'
+    ],
+    darkMode: false,
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
